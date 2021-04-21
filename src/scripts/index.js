fc9dfb8fc679a4a21e82797d553633465196f3bb
meriemkhadraoui <miyakhad@gmail.com>
2021-04-21 11:59:07 +0200

    javascript in the browse

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,27 @@ function change() {
     "<h1>Hi this is new Text from JS</h1>";
   mainTexe.style.backgroundColor = "gray";
 }
+function multiply() {
+  let userNum = document.querySelector("#num").value;
+  // console.log(userNum);
+  let resultText = "";
+  for (let i = 1; i <= 10; i++) {
+    resultText += `${i} * ${userNum} = ${i * userNum} <br>`;
+  }
+  document.querySelector(".result").innerHTML = resultText;
+}
+
+function subtraction() {
+  let hoursWork = document.querySelector("#hours").value;
+  let hourWorkMonth = hoursWork * 5 * 4;
+  let standerHours = 160;
+  let result = standerHours - hourWorkMonth;
+  let result1 = "";
+  if (standerHours > hourWorkMonth) {
+    result1 = `<h2> you worked ${hourWorkMonth} Hours in month you need to work ${result} hours more</h2> `;
+  } else {
+    result1 = `<h2> you worked ${-result} Hours overtime  you can take vacation </h2>`;
+  }
+
+  document.querySelector(".result1").innerHTML = result1;
+}
